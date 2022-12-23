24c155528bc2f0fb6e02129c614c3c2338734800
Martino Viganò <martino.vigano@cegeka.com>
2022-12-23 16:24:45 +0100

    Add Tovarish TITLE

--- xmas.py
+++ xmas.py
@@ -1,6 +1,60 @@
 from turtle import *
 
-i=Turtle()
-s=Screen()
-s.title("BUON NATALE MERDE")
-s.exitonclick()
+hideturtle()
+title("BUON NATALE.....")
+color('green','green')
+speed(30)
+penup()
+goto(0,300)
+begin_fill()
+pendown()
+right(30)
+
+for i in range (0,20):
+    forward(50)
+    right(150)
+    forward(30)
+    left(150)
+
+right(150)
+
+p1=xcor()
+p2=ycor()
+goto(-p1-30,p2)
+
+for i in range (0,19):
+    right(150)
+    forward(50)
+    left(150)
+    forward(30)
+
+right(150)
+forward(50)
+
+end_fill()
+
+
+penup()
+
+
+p1=xcor()
+p2=ycor()
+
+goto(p1-20,p2+16)
+showturtle()
+color('red','red')
+pendown()
+title("BUON NATALE.....TOVARISH!")
+speed(5)
+begin_fill()
+color('red','red')
+right(30)
+right(180-144)
+for k in range(5):
+		forward(40)
+		left(144)
+hideturtle()
+end_fill()
+
+
+done()
